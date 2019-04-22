3590ea1f3ab7f059df9751ee6cf007141bf6f3ac
lauwba <febri4api@gmail.com>
2019-04-22 09:11:30 +0700

    detail

--- app/src/main/java/com/febri/blogs/ContentList.kt
+++ app/src/main/java/com/febri/blogs/ContentList.kt
@@ -8,51 +8,48 @@ import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import com.febri.blogs.config.Config
 import com.febri.blogs.model.ContentModel
-import com.febri.blogs.model.SubKategoriModel
 import com.febri.blogs.service.RequestHandler
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.json.JSONObject
 
 class ContentList : AppCompatActivity() {
 
-    private var id:String?=null
-    private var pd: ProgressDialog?=null
-    private var list:MutableList<ContentModel>?=null
+    private var id: String? = null
+    private var pd: ProgressDialog? = null
+    private var list: MutableList<ContentModel>? = null
     private var data: HashMap<String, String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contentlist_activity)
 
-        id=intent.getStringExtra(Config.id)
+        id = intent.getStringExtra(Config.id)
 
         data = hashMapOf()
 
-        list= mutableListOf()
+        list = mutableListOf()
         showData().execute()
-
-
     }
+
     inner class showData : AsyncTask<String, Void, String>() {
         override fun onPreExecute() {
             super.onPreExecute()
-            pd= ProgressDialog.show(this@ContentList,"","Wait",true,true)
+            pd = ProgressDialog.show(this@ContentList, "", "Wait", true, true)
         }
 
         override fun doInBackground(vararg params: String?): String {
             val handler = RequestHandler()
-            val result=handler.sendGetRequest(Config.url_content+id)
+            val result = handler.sendGetRequest(Config.url_content + id)
             return result
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             pd?.dismiss()
-            val objek= JSONObject(result)
-            if (objek.getInt("status")==1){
+            val objek = JSONObject(result)
+            if (objek.getInt("status") == 1) {
                 Toast.makeText(this@ContentList, "Tidak ada data!", Toast.LENGTH_SHORT).show()
-            }
-            else {
+            } else {
                 Toast.makeText(this@ContentList, "ada data!", Toast.LENGTH_SHORT).show()
                 val array = objek.getJSONArray("data")
                 for (i in 0 until array.length()) {
---
+++ app/src/main/java/com/febri/blogs/Detail.kt
@@ -0,0 +1,10 @@
+package com.febri.blogs
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Detail : AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
